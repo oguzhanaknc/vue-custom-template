5bf8ed2a76542f5e4b474c2d245eb01faf93bdad
oguzhan <oguzzhanakinci@gmail.com>
2020-09-09 18:39:29 +0300

    initial

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+   <h1> Anasayfa</h1>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "Home",
+};
+</script>
+<style  scoped>
+h1{
+  color: rebeccapurple;
+}
+ @media(--t){
+   h1{
+     color: royalblue;
+   }
+ }
+</style>
